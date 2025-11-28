61e57bce7c62435ccb173dff934ae7d0d5aa49cf
Guilhem Gerouille <guilhem.gerouille@etu.unistra.fr>
2025-11-28 21:19:36 +0100

    temp commit

--- data.py
+++ data.py
@@ -1,16 +1,11 @@
 import datetime as dt
 
 class Round:
-    def __init__(self, *, winner: str|None = None, discarder: str|None = None, hand_points: int, penalties: dict[str, int]|None = None, data_dict: dict|None = None):
-        if data_dict is None:
-            self.winner = winner
-            self.discarder = discarder
-            self.hand_points = hand_points
-            if penalties is None:
-                penalties = {}
-            self.penalties = penalties
-        else:
-            self.from_dict(data_dict)
+    def __init__(self, winner: str|None, discarder: str|None, hand_points: int, penalties: dict[str, int]|None = {}):
+        self.winner = winner
+        self.discarder = discarder
+        self.hand_points = hand_points
+        self.penalties = penalties
     
     def to_dict(self) -> dict:
         return {
@@ -20,11 +15,13 @@ class Round:
             "penalties": self.penalties
         }
 
-    def from_dict(self, data: dict):
-        self.winner = data['winner']
-        self.discarder = data['discarder']
-        self.hand_points = data['hand_points']
-        self.penalties = data['penalties']
+    @classmethod
+    def from_dict(self, data: dict) -> "Round":
+        return Round(winner = data['winner'],
+            discarder = data['discarder'],
+            hand_points = data['hand_points'],
+            penalties = data['penalties']
+        )
 
     def __str__(self):
         return f"winner={self.winner}, discarder={self.discarder}, points={self.hand_points}, penalties={self.penalties}"
@@ -40,15 +37,17 @@ class Game:
         return {
             "players": self.players,
             "end_points": self.end_points,
-            "date": self.date,
-            "rounds": self.rounds
+            "date": self.date.isoformat(),
+            "rounds": [r.to_dict() for r in self.rounds]
         }
     
-    def from_dict(self, data: dict):
-        self.players = data['players']
-        self.end_points = data['end_points']
-        self.date = data['date']
-        self.rounds = data['rounds']
+    @classmethod
+    def from_dict(self, data: dict) -> "Game":
+        return Game(players = data['players'],
+            end_points = data['end_points'],
+            date = dt.datetime.fromisoformat(data['date']),
+            rounds = [data['rounds']]
+        )
 
 class Player:
     def __init__(self, name: str, base_elo: float):
